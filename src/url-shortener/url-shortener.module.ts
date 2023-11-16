b972992980dac25767fb8d2dde385ea2f683773a
Rubén Villar Grela <ruben@rubenvillar.net>
2023-11-16 14:28:45 +0100

    created url shortener service

--- src/url-shortener/url-shortener.module.ts
+++ src/url-shortener/url-shortener.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { UrlShortenerService } from './url-shortener.service';
 import { UrlShortenerController } from './url-shortener.controller';
 
 @Module({
+  imports: [HttpModule],
   providers: [UrlShortenerService],
-  controllers: [UrlShortenerController]
+  controllers: [UrlShortenerController],
 })
 export class UrlShortenerModule {}
--- src/url-shortener/url-shortener.service.ts
+++ src/url-shortener/url-shortener.service.ts
@@ -1,4 +1,22 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+interface CleanUriAPIResponse {
+  result_url: string;
+}
 
 @Injectable()
-export class UrlShortenerService {}
+export class UrlShortenerService {
+  constructor(private httpService: HttpService) {}
+
+  async shortenUrl(originalUrl: string): Promise<string> {
+    const response = await firstValueFrom(
+      this.httpService.post<CleanUriAPIResponse>(
+        'https://cleanuri.com/api/v1/shorten',
+        { url: originalUrl },
+      ),
+    );
+    return response.data.result_url;
+  }
+}
