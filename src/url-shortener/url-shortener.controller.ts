9028da299edfd10ac13c98fc427ce1282e75de7e
Rubén Villar Grela <ruben@rubenvillar.net>
2023-11-16 14:29:02 +0100

    created url shortener module

--- src/url-shortener/url-shortener.controller.ts
+++ src/url-shortener/url-shortener.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { UrlShortenerService } from './url-shortener.service';
+import { Response } from 'express';
 
 @Controller('url-shortener')
-export class UrlShortenerController {}
+export class UrlShortenerController {
+  constructor(private readonly urlShortenerService: UrlShortenerService) {}
+
+  @Post()
+  async shortenUrl(
+    @Body('url') url: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      const shortUrl = await this.urlShortenerService.shortenUrl(url);
+      res.status(HttpStatus.OK).json({ shortUrl });
+    } catch (err) {
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: err.message });
+    }
+  }
+}
